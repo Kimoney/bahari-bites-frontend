6a97652604994337bb221f85b19dc6a1758691ce
John <johntaykimani@gmail.com>
2024-06-10 13:24:58 +0300

    feat: Improved the footer and matched it to theme color.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
-// src/components/Footer.js
-// import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 p-4 text-center text-white">
-      <p>&copy; 2024 My Simple React App</p>
+    <footer className="p-4 text-white flex justify-between items-center">
+      <div className="flex items-center space-x-4">
+        <a href="#" className="text-orange-500 hover:text-orange-400">
+          <FaFacebook />
+        </a>
+        <a href="#" className="text-orange-500 hover:text-orange-400">
+          <FaTwitter />
+        </a>
+        <a href="#" className="text-orange-500 hover:text-orange-400">
+          <FaInstagram />
+        </a>
+      </div>
+      <p className="text-orange-500 font-bold">&copy; 2024 Bahari Bites</p>
+      <div className="flex items-center space-x-4">
+        <a href="#" className="text-orange-500 hover:text-orange-400">FAQ</a>
+        <a href="#" className="text-orange-500 hover:text-orange-400">Terms & Conditions</a>
+        <a href="#" className="text-orange-500 hover:text-orange-400">Careers</a>
+      </div>
     </footer>
   );
 };
