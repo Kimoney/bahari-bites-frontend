c8531fb80c76fa84ad44b8e82661470095dae3fd
John <johntaykimani@gmail.com>
2024-06-11 08:27:04 +0300

    chore: Installed React Scroll.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,23 +2,23 @@ import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="p-4 text-white flex justify-between items-center">
+    <footer className="p-2 text-white flex justify-between items-center">
       <div className="flex items-center space-x-4">
-        <a href="#" className="text-orange-500 hover:text-orange-400">
+        <a href="#" className="text-black hover:text-orange-400">
           <FaFacebook />
         </a>
-        <a href="#" className="text-orange-500 hover:text-orange-400">
+        <a href="#" className="text-black hover:text-orange-400">
           <FaTwitter />
         </a>
-        <a href="#" className="text-orange-500 hover:text-orange-400">
+        <a href="#" className="text-black hover:text-orange-400">
           <FaInstagram />
         </a>
       </div>
       <p className="text-orange-500 font-bold">&copy; 2024 Bahari Bites</p>
       <div className="flex items-center space-x-4">
-        <a href="#" className="text-orange-500 hover:text-orange-400">FAQ</a>
-        <a href="#" className="text-orange-500 hover:text-orange-400">Terms & Conditions</a>
-        <a href="#" className="text-orange-500 hover:text-orange-400">Careers</a>
+        <a href="#" className="text-black hover:text-orange-400">FAQ</a>
+        <a href="#" className="text-black hover:text-orange-400">Terms & Conditions</a>
+        <a href="#" className="text-black hover:text-orange-400">Careers</a>
       </div>
     </footer>
   );
