50062b93d48ede96679746870daea527f9144d78
John <johntaykimani@gmail.com>
2024-06-12 22:13:44 +0300

    feat: Added routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-// import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './components/HomePage/HomePage';
+import Menu from './components/Menu/Menu';
 import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <HomePage className="flex-grow" />
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+     <>
+     <h1>Menu</h1>
+     <h1>Menu</h1>
+     <h1>Menu</h1>
+     <h1>Menu</h1>
+     <h1>Menu</h1>
+     <h1>Menu</h1>
+     </>
+    );
+  };
+  
+  export default Footer;
+  
