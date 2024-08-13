9bae73d0c5c0359923c1f4d0ad61584b7cb62cf9
John <johntaykimani@gmail.com>
2024-08-13 17:06:33 +0300

    style: Changed flex on footer for small devices.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
 
   return (
     <footer className="p-4">
-      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
-        <div className="flex items-center space-x-4 mb-4 sm:mb-0">
+      <div className="container mx-auto flex sm:flex-row justify-between items-center">
+        <div className="flex items-center md:space-x-4 space-x-2 mb-4 sm:mb-0">
           <a href="#" className="hover:text-orange-400">
             <FaFacebook />
           </a>
@@ -18,10 +18,10 @@ const Footer = () => {
             <FaInstagram />
           </a>
         </div>
-        <p className="text-orange-500 font-bold mb-4 sm:mb-0">
+        <p className="text-orange-500 font-bold mb-4 sm:mb-0 whitespace-nowrap">
           &copy; {currentYear} Bahari Bites
         </p>
-        <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4">
+        <div className="whitespace-nowrap flex flex-col sm:flex-row items-right space-y-2 sm:space-y-0 sm:space-x-4">
           <a href="#" className="hover:text-orange-400">FAQ</a>
           <a href="#" className="hover:text-orange-400">Terms & Conditions</a>
           <a href="#" className="hover:text-orange-400">Careers</a>
