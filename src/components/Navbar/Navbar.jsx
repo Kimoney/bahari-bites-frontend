62592cc12564209c9a7cd46422023f7f28849908
John <johntaykimani@gmail.com>
2024-08-13 18:59:22 +0300

    feat: added dropdown using headles ui.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { NavLink, useLocation, Link } from 'react-router-dom';
 import { FaShoppingCart, FaUser, FaHome, FaUtensils, FaInfoCircle, FaPhone, FaReceipt } from 'react-icons/fa';
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/react';
 import { TbShoppingCartQuestion } from "react-icons/tb";
 import { useCart } from '../../context/CartContext';
 import baharilogo from '../../assets/logo/baharilogo.png';
@@ -119,7 +120,7 @@ const Navbar = () => {
           </NavLink>
         </li>
       </ul>
-      <div className="flex space-x-5 relative ml-3">
+      <div className="flex space-x-5 relative ml-3 mr-3">
         <NavLink to="/cart" onClick={() => setActiveSection('cart')}>
           <div className="relative">
             <FaShoppingCart 
@@ -133,7 +134,48 @@ const Navbar = () => {
           )}
         </div>
       </NavLink>
-      <FaUser className="text-black hover:text-orange-500 cursor-pointer" size={20} />
+      
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+        <MenuButton className="">
+          <FaUser className="text-black hover:text-orange-500 cursor-pointer" size={20} />
+        </MenuButton>
+      </div>
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="/profile"
+              className="block px-4 py-2 text-sm hover:bg-orange-100"
+            >
+              Profile
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="/help"
+              className="block px-4 py-2 text-sm hover:bg-orange-100"
+            >
+              Help
+            </a>
+          </MenuItem>
+          <form action="#" method="POST">
+            <MenuItem>
+              <button
+                type="submit"
+                className="block w-full px-4 py-2 text-left text-sm hover:bg-orange-100"
+              >
+                Logout
+              </button>
+            </MenuItem>
+          </form>
+        </div>
+      </MenuItems>
+    </Menu>
     </div>
     </nav>
   );
