c61be8bd763dc0cb3e872e52b9600aab6e9a14fa
John <johntaykimani@gmail.com>
2024-08-14 14:48:57 +0300

    style: homepage styling complete.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import TodaysSpecial from '../TodaysSpecial/TodaysSpecial';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
     <>
       <div
         className="h-screen bg-cover bg-center flex items-center"
-        style={{ backgroundImage: `url('https://images.pexels.com/photos/1150447/pexels-photo-1150447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')` }}
+        style={{ backgroundImage: `url('https://images.pexels.com/photos/3192171/pexels-photo-3192171.jpeg')` }}
       >
         <div className="container mx-auto px-6 text-left">
           <div className="animate-slide-in">
-            <h1 className="text-5xl font-bold text-orange-100 mb-4">
+            <h1 className="text-5xl font-bold  mb-4">
               Unleash Your <span className='text-orange-500'>Taste Buds</span> With<br />
               Our <span className='text-orange-500'>Seafood</span> Delights
             </h1>
-            <p className="text-lg text-orange-100 mb-8">
+            <p className="text-lg mb-8">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br />
               sed do eiusmod tempor incididunt ut labore et dolore.
             </p>
             <div>
-              <button className="bg-orange-500 text-white py-2 px-6 rounded-full mr-4">
+              <Link to="/menu" className="border border-orange-500 hover:text-black hover:bg-orange-500 py-2 px-6 rounded-full mr-4">
                 Order Now
-              </button>
-              <button className="bg-orange-500 text-white py-2 px-6 rounded-full">
+              </Link>
+              <Link to="/howtoorder" className="border border-orange-500 hover:text-black hover:bg-orange-500 py-2 px-6 rounded-full">
                 How to Order
-              </button>
+              </Link>
             </div>
           </div>
         </div>
